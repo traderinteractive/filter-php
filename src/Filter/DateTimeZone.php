d79d0ef0339f82e28897a2329106798b7c68c710
Chad Gray <chadwickgray@gmail.com>
2014-10-10 11:40:34 -0400

    Add Filter\DateTimeZone

---
+++ src/Filter/DateTimeZone.php
@@ -0,0 +1,40 @@
+<?php
+namespace DominionEnterprises\Filter;
+
+/**
+ * A collection of filters for filtering strings into \DateTimeZone objects.
+ */
+class DateTimeZone
+{
+    /**
+     * Filters the given value into a \DateTimeZone object.
+     *
+     * @param mixed   $value     The value to be filtered.
+     * @param boolean $allowNull True to allow nulls through, and false (default) if nulls should not be allowed.
+     *
+     * @return \DateTimeZone
+     *
+     * @throws \InvalidArgumentException Thrown if $allowNull was not a boolean value.
+     * @throws \Exception if the value did not pass validation.
+     */
+    public static function filter($value, $allowNull = false)
+    {
+        if ($allowNull !== false && $allowNull !== true) {
+            throw new \InvalidArgumentException('$allowNull was not a boolean value');
+        }
+
+        if ($value === null && $allowNull) {
+            return null;
+        }
+
+        if ($value instanceof \DateTimeZone) {
+            return $value;
+        }
+
+        if (!is_string($value) || trim($value) == '') {
+            throw new \Exception('$value not a non-empty string');
+        }
+
+        return new \DateTimeZone($value);
+    }
+}
